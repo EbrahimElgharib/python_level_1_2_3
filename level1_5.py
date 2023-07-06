54136f9ec6966c7f43b39273be0c0c2518224ee3
Ebrahim Elsayed Elgharib <ebrahimelsayed010.com>
2023-07-07 00:54:49 +0300

    edit comments and structure only in level1_5.py

--- level1_5.py
+++ level1_5.py
@@ -1,22 +1,23 @@
-# Python Task Level, 4 :
-# Create a function that takes a sentence and prints the sentence without duplicated words
+# Python Task Level, 5 :
+# Create a function that takes a sentence and prints how many words in the sentence (do not count the spaces)
 
 
+def count_words(sentence):
+    # output sentence without a duplicated words
+    count_words = 0
+    ha = True
+
+    for w in sentence:
+        if w != ' ' and ha: # w is a letter not space
+            count_words += 1
+            ha = False
+        elif w == ' ':
+            ha = True
+
+    print(f'Count of words is: {count_words}')
+
+        
+        
 # get string
-my_sentence = input('Please type a Sentence : ')
-
-# output sentence without a duplicated words
-count_words = 0
-ha = True
-
-for w in my_sentence:
-    if w != ' ' and ha: # w is a letter not space
-        count_words += 1
-        ha = False
-    elif w == ' ':
-        ha = True
-
-print(f'Count of words is: {count_words}')
-
-        
-        
+sentence = input('Please type a Sentence : ')
+count_words(sentence)
