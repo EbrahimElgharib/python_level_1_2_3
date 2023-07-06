71ad51c46e3fba1e7aa1d4381b3ffcfa39b2bdef
Ebrahim Elsayed Elgharib <ebrahimelsayed010.com>
2023-07-07 01:17:51 +0300

    edit level1_8.py V2

--- level1_8.py
+++ level1_8.py
@@ -2,25 +2,33 @@
 # Create a function that takes x,y and prints all the number that can be divide by y from x to 100
 
 
+
+def func(x, y):
+    # output
+    print("All numbers that can be divided on {y} from {x} to 100 :")
+
+    for n in range(x,101):
+        if n%y == 0:    print(n)
+        
+        
 # get two numbers x,y
 while True:
     
     try:
         x, y = int(input("type number1: ")), int(input("type number2: "))
+        _ = 1 / y   # check divide by zero Exception
         break
         
     except Exception:
         print("try a valid int number please.")
+        
+func(x, y)
 
     
-# output
-print("All numbers that can be divided on {y} from {x} to 100 :")
-if x >= y:
-    for n in range(x,101):
-        if n%y == 0:
-            print(n)
-else:
-    print("Nothing")
+
+        
+
+            
 
     
         
